f7a3e35b0f3a9a67eab8232dc6801f42b6345fd4
Najmulkhan1 <najmulislam624@gmail.com>
2025-11-28 13:35:41 +0600

    footer add

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import AuthProvider from "@/providers/AuthProvider";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({ children }) {
         <AuthProvider>
           <Navbar></Navbar>
           {children}
+          <Footer></Footer>
         </AuthProvider>
       </body>
     </html>
